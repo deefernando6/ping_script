d4908f45d86a9d5959be37df1acd780bbc251104
deeghayu <deefernando6@gmail.com>
2021-06-24 21:15:29 +0530

    Update ping_script.py

--- ping_script.py
+++ ping_script.py
@@ -3,3 +3,9 @@ import platform
 
 print('Type ip address which you want to ping: ')
 host = input()
+
+def ping(host):
+
+    #par = '-n' if platform.system().lower == 'windows' else '-c'
+    if [platform.system().lower == 'windows']:
+        par = '-n'
